9174f697dd634c26fe9bdba254e2af294df7c9b9
Давлат Аблязов < >
2017-04-23 20:18:23 +0300

    Add script for run

---
+++ RunSetSample.ps1
@@ -0,0 +1,4 @@
+﻿for ($i = 0; $i -lt 10; $i++){
+$param = ($i+1)*1000
+$process = Start-Process -FilePath "D:\AVL\AVL-Tree\sln\SearchTrees\Release\SetSample.exe" -ArgumentList $param -NoNewWindow -Wait
+}
